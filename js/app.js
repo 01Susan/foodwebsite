7b9549d6f74bfcdc08b3dad272641d1179bc4646
susan <dhitalsusan2@gmail.com>
2021-11-22 07:21:00 +0545

    new website added

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".food-slider").slick({
+        autoplay: true,
+        slidesToShow: 3,
+        prevArrow:".prev-btn",
+        nextArrow:".next-btn",
+
+    });
+});
